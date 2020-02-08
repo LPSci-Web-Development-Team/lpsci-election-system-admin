9c9fdc13f40bcb65b6baa6ca26a0126a66c989b8
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-02-08 14:45:00 +0800

    Add sidebar scoped models

--- components/utils/Providers.tsx
+++ components/utils/Providers.tsx
@@ -1,3 +1,13 @@
-export const makeProviders = () => [
+// ANCHOR React
+import * as React from 'react';
 
+// ANCHOR Models
+import { SidebarSection } from '@lpsci/scoped-models/sidebar-sections/SidebarSections';
+
+export const makeProviders = () => [
+  /*
+  ANCHOR: Provider for the SidebarSection
+  NOTE: Used for providing the list of sections in the sidebar
+  */
+  <SidebarSection.Provider key="SignInFormInput" />,
 ];
