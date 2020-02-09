9486ea9c518aa4b8a27de64f3ff96b1a818ea6b6
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-02-09 10:38:03 +0800

    Update global providers

--- components/utils/Providers.tsx
+++ components/utils/Providers.tsx
@@ -5,6 +5,8 @@ import * as React from 'react';
 import { SidebarSection } from '@lpsci/scoped-models/sidebar-sections/SidebarSections';
 import { SignInFormInput } from '@lpsci/scoped-models/sign-in/SignInFormInput';
 import { VoteModal } from '@lpsci/scoped-models/vote-modal/VoteModal';
+import { ForgotSearch } from '@lpsci/scoped-models/forgot-search/ForgotSearch';
+import { ResetModal } from '@lpsci/scoped-models/reset-modal/ResetModal';
 
 export const makeProviders = () => [
   /*
@@ -22,4 +24,14 @@ export const makeProviders = () => [
   NOTE: Used for toggling modal for voter's vote
   */
   <VoteModal.Provider key="VoteModal" />,
+  /*
+  ANCHOR: Provider for the ForgotSearch
+  NOTE: Used for search users in the forgot page
+  */
+  <ForgotSearch.Provider key="ForgotSearch" />,
+  /*
+  ANCHOR: Provider for the ResetModal
+  NOTE: Used for toggling modal for resetting passwords
+  */
+  <ResetModal.Provider key="ResetModal" />,
 ];
