6ee2f369ec812a886656d930ed7496bbdc5598e3
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-02-09 10:37:10 +0800

    Add page for forgot password

---
+++ pages/forgot.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-default-export */
+// ANCHOR React
+import * as React from 'react';
+
+// ANCHOR Components
+import { GlobalProvider } from '@lpsci/components/GlobalProvider';
+import { ElectionAppHead } from '@lpsci/components/head/ElectionAppHead/ElectionAppHead';
+import { ElectionPage } from '@lpsci/components/ElectionPrivatePage';
+import { ElectionForgot } from '@lpsci/components/forgot/ElectionForgot';
+
+export default React.memo(() => (
+  <GlobalProvider>
+    <ElectionAppHead title="SSG Election" description="Election Admin App" />
+    <ElectionPage>
+      <ElectionForgot />
+    </ElectionPage>
+  </GlobalProvider>
+));
