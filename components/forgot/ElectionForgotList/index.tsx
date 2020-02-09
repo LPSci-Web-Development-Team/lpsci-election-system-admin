4416381b0d7af2c0418e9196b7499297c5b3e2e7
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-02-09 11:11:13 +0800

    Add conditional styling on tags

--- components/forgot/ElectionForgotList/index.tsx
+++ components/forgot/ElectionForgotList/index.tsx
@@ -1,16 +1,22 @@
+/* eslint-disable no-mixed-operators */
 // ANCHOR React
 import * as React from 'react';
 
 // ANCHOR Base
 import { styled } from 'baseui';
 import { Button } from 'baseui/button';
-import { Tag, VARIANT } from 'baseui/tag';
+import { Tag, VARIANT, KIND } from 'baseui/tag';
 import { ListItem, ListItemLabel } from 'baseui/list';
 import { Paragraph1, Paragraph2 } from 'baseui/typography';
 import { Block } from 'baseui/block';
 
-// ANCHOR Styles
+// ANCHOR Scoped Models
 import { ResetModal } from '@lpsci/scoped-models/reset-modal/ResetModal';
+
+// ANCHOR UI Models
+import { VOTER_ROW } from 'models/ui-models/voter-datatable/rows';
+
+// ANCHOR Styles
 import {
   TAG, CONTAINER, LAST_NAME, FIRST_NAME,
 } from './styles';
@@ -36,29 +42,55 @@ export const ElectionForgotList = React.memo(() => {
 
   return (
     <ListContainer>
-      <ListItem
-        artwork={() => (
-          <Tag overrides={TAG} closeable={false} variant={VARIANT.solid} kind="primary">
-            Faraday
-          </Tag>
-        )}
-        endEnhancer={() => (
-          <Button
-            size="compact"
-            kind="primary"
-            onClick={() => openModal('Prince Neil Cedrick Castro')}
-          >
-            Reset Password
-          </Button>
-        )}
-      >
-        <ListItemLabel>
-          <Block overrides={CONTAINER}>
-            <Paragraph1 overrides={LAST_NAME}>CASTRO,</Paragraph1>
-            <Paragraph2 overrides={FIRST_NAME}>Prince Neil Cedrick</Paragraph2>
-          </Block>
-        </ListItemLabel>
-      </ListItem>
+      {VOTER_ROW.map(({ id, data }) => (
+        <ListItem
+          key={id}
+          artwork={() => (
+            <Tag
+              overrides={TAG}
+              closeable={false}
+              variant={
+                data.gradeLevel === 12
+                  ? VARIANT.outlined
+                  : VARIANT.solid
+              }
+              kind={
+                data.gradeLevel === 11 || data.gradeLevel === 12
+                  ? KIND.primary
+                  : KIND.custom
+              }
+              color={
+                data.gradeLevel === 7 && '#00CF91'
+                || data.gradeLevel === 8 && '#FFD600'
+                || data.gradeLevel === 9 && '#EE3D48'
+                || data.gradeLevel === 10 && '#1EC9E8'
+                || 'black'
+            }
+            >
+              {data.section}
+            </Tag>
+          )}
+          endEnhancer={() => (
+            <Button
+              size="compact"
+              kind="primary"
+              onClick={() => openModal(data.firstName)}
+            >
+              Reset Password
+            </Button>
+          )}
+        >
+          <ListItemLabel>
+            <Block overrides={CONTAINER}>
+              <Paragraph1 overrides={LAST_NAME}>
+                {data.lastName.toUpperCase()}
+                ,
+              </Paragraph1>
+              <Paragraph2 overrides={FIRST_NAME}>{data.firstName}</Paragraph2>
+            </Block>
+          </ListItemLabel>
+        </ListItem>
+      ))}
     </ListContainer>
   );
 });
