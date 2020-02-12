93d6ffef2dc4f88ebd3a6205003dfc61d8464b96
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-02-12 09:41:57 +0800

    Add payload, api, and handler for candidates post

--- scoped-models/candidates-modal/CandidatesModal.tsx
+++ scoped-models/candidates-modal/CandidatesModal.tsx
@@ -12,10 +12,38 @@ export const CandidatesModal = createModel(() => {
   const [firstName, setFirstName] = React.useState<string>('');
   const [lastName, setLastName] = React.useState<string>('');
   const [position, setPosition] = React.useState<string>('');
-  const [party, setParty] = React.useState<any>([]);
+  const [party, setParty] = React.useState<string>('');
   const [image, setImage] = React.useState<string>('');
 
+  const handler = useConstant(() => ({
+    firstName: (e: React.ChangeEvent<HTMLInputElement>) => {
+      setFirstName(e.target.value);
+    },
+    lastName: (e: React.ChangeEvent<HTMLInputElement>) => {
+      setLastName(e.target.value);
+    },
+    positiom: (e: React.ChangeEvent<HTMLInputElement>) => {
+      setPosition(e.target.value);
+    },
+    party: (e: React.ChangeEvent<HTMLInputElement>) => {
+      setParty(e.target.value);
+    },
+    image: (e: React.ChangeEvent<HTMLInputElement>) => {
+      setImage(e.target.value);
+    },
+  }));
+
+  const filled = React.useMemo(() => (
+    firstName !== ''
+    && lastName !== ''
+    && position !== ''
+    && party !== ''
+    && image !== ''
+  ), [firstName, lastName, position, party, image]);
+
   return {
+    handler,
+    filled,
     addModal,
     setAddModal,
     editModal,
---
+++ utils/payloads/candidate.ts
@@ -0,0 +1,7 @@
+export interface ICandidatePayload {
+  readonly firstName: string;
+  readonly lastName: string;
+  readonly position: string;
+  readonly party: string;
+  readonly image: string;
+}
