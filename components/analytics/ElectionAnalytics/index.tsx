d757ab7f2167b71a8272471f380187b124051bb7
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-03-30 06:54:15 +0800

    Remove tally votes

--- components/analytics/ElectionAnalytics/index.tsx
+++ components/analytics/ElectionAnalytics/index.tsx
@@ -4,12 +4,10 @@ import * as React from 'react';
 // ANCHOR Components
 import { ElectionAnalyticsHeading } from '../ElectionAnalyticsHeading';
 import { ElectionAnalyticsBar } from '../ElectionAnalyticsBar';
-import { ElectionTallyVotes } from '../ElectionTallyVotes';
 
 export const ElectionAnalytics = React.memo(() => (
   <>
     <ElectionAnalyticsHeading />
     <ElectionAnalyticsBar />
-    <ElectionTallyVotes />
   </>
 ));
