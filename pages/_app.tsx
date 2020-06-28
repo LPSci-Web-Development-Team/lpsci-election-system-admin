ac95f9841f419c0f0f0c6a5da7d497de7640aff5
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-06-28 16:11:07 +0800

    Add _app and _document pages

---
+++ pages/_app.tsx
@@ -0,0 +1,26 @@
+// ANCHOR Next
+import App from 'next/app';
+
+// ANCHOR React
+import * as React from 'react';
+
+// ANCHOR Component
+import { GlobalProvider } from '../components/GlobalProvider';
+
+// ANCHOR CSS
+import 'normalize.css/normalize.css';
+import '../public/styles/parsed-link.css';
+
+// NOTE Next App requires this to be exported by default
+// eslint-disable-next-line import/no-default-export
+export default class LpsciApp extends App {
+  public render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <GlobalProvider>
+        <Component {...pageProps} />
+      </GlobalProvider>
+    );
+  }
+}
