3696c06b9477608748b19324e4ff1a29f60aae5d
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-06-29 01:56:29 +0800

    Add response error

--- utils/firebase/hooks/useAuthSignIn.tsx
+++ utils/firebase/hooks/useAuthSignIn.tsx
@@ -8,30 +8,22 @@ export function useAuthSignIn() {
   // get instance
   const authInstance = FirebaseAuth.useSelector((state) => state.auth);
 
-  const [loading, setLoading] = React.useState(false);
-  const [error, setError] = React.useState<any>();
-
   const signinCore = React.useCallback(async (email: string, password: string) => {
     if (authInstance) {
       try {
         await authInstance.signInWithEmailAndPassword(email, password);
       } catch (err) {
-        setError(err);
+        throw new Error(err);
       }
-      setLoading(false);
     }
   }, [authInstance]);
 
   const call = React.useCallback(async (email: string, password: string) => {
-    setLoading(true);
-
     if (authInstance && authInstance.currentUser) {
       try {
         await authInstance.signOut();
       } catch (err) {
-        setError(err);
-        setLoading(false);
-        return;
+        throw new Error(err);
       }
     }
 
@@ -39,8 +31,6 @@ export function useAuthSignIn() {
   }, [authInstance, signinCore]);
 
   return React.useMemo(() => ({
-    loading,
-    error,
     call,
-  }), [loading, error, call]);
+  }), [call]);
 }
