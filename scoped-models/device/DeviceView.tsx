0573eb549dfe5d0a430f797f1515fbe1b5aba705
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-06-28 18:25:20 +0800

    Create DeviceView.tsx

---
+++ scoped-models/device/DeviceView.tsx
@@ -0,0 +1,26 @@
+// ANCHOR Model
+import createModel from '@lxsmnsyc/react-scoped-model';
+
+// ANCHOR Hooks
+import { useWindowSize } from '@lpsci/hooks';
+
+interface IState {
+  isMobile: boolean;
+  isDesktop: boolean;
+  width: number;
+  height: number;
+}
+
+export const DeviceView = createModel<IState>(() => {
+  const { width, height } = useWindowSize();
+
+  return {
+    isMobile: width <= 768,
+    isDesktop: width > 768,
+    width,
+    height,
+  };
+},
+{
+  displayName: 'Models.DeviceView',
+});
