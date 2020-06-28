ddcefc7874c1225a5f83c1bd74b30842fd598159
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-06-29 00:49:05 +0800

    Add firebase methods

---
+++ utils/firebase/credentials.ts
@@ -0,0 +1,10 @@
+export const CREDENTIALS = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
