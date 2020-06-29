1107b212727231f02db0734ee83c02f98c6abc63
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-06-29 21:09:32 +0800

    Add shadow themes

---
+++ utils/themes/shadows.ts
@@ -0,0 +1,62 @@
+export const SHADOWS = {
+  shadow400: `
+    0 1px 1px rgba(0,0,0,0.125),
+    0 2px 2px rgba(0,0,0,0.125)
+  `,
+  shadow500: `
+    0 1px 1px rgba(0,0,0,0.125),
+    0 2px 2px rgba(0,0,0,0.125),
+    0 4px 4px rgba(0,0,0,0.125)
+  `,
+  shadow600: `
+    0 1px 1px rgba(0,0,0,0.125),
+    0 2px 2px rgba(0,0,0,0.125),
+    0 4px 4px rgba(0,0,0,0.125),
+    0 8px 8px rgba(0,0,0,0.125)
+  `,
+  shadow700: `
+    0 1px 1px rgba(0,0,0,0.125),
+    0 2px 2px rgba(0,0,0,0.125),
+    0 4px 4px rgba(0,0,0,0.125),
+    0 8px 8px rgba(0,0,0,0.125),
+    0 16px 16px rgba(0,0,0,0.125)
+  `,
+  shadow800: `
+    0 1px 1px rgba(0,0,0,0.125),
+    0 2px 2px rgba(0,0,0,0.125),
+    0 4px 4px rgba(0,0,0,0.125),
+    0 8px 8px rgba(0,0,0,0.125),
+    0 16px 16px rgba(0,0,0,0.125),
+    0 32px 32px rgba(0,0,0,0.125)
+  `,
+  tshadow400: `
+    0 -1px 1px rgba(0,0,0,0.125),
+    0 -2px 2px rgba(0,0,0,0.125)
+  `,
+  tshadow500: `
+    0 -1px 1px rgba(0,0,0,0.125),
+    0 -2px 2px rgba(0,0,0,0.125),
+    0 -4px 4px rgba(0,0,0,0.125)
+  `,
+  tshadow600: `
+    0 -1px 1px rgba(0,0,0,0.125),
+    0 -2px 2px rgba(0,0,0,0.125),
+    0 -4px 4px rgba(0,0,0,0.125),
+    0 -8px 8px rgba(0,0,0,0.125)
+  `,
+  tshadow700: `
+    0 -1px 1px rgba(0,0,0,0.125),
+    0 -2px 2px rgba(0,0,0,0.125),
+    0 -4px 4px rgba(0,0,0,0.125),
+    0 -8px 8px rgba(0,0,0,0.125),
+    0 -16px 16px rgba(0,0,0,0.125)
+  `,
+  tshadow800: `
+    0 -1px 1px rgba(0,0,0,0.125),
+    0 -2px 2px rgba(0,0,0,0.125),
+    0 -4px 4px rgba(0,0,0,0.125),
+    0 -8px 8px rgba(0,0,0,0.125),
+    0 -16px 16px rgba(0,0,0,0.125),
+    0 -32px 32px rgba(0,0,0,0.125)
+  `,
+};
