b9d6c83cdd47b8e01288c0dddc0ac7bcbc5bd880
Prince Neil Cedrick Castro <46030248+git-ced@users.noreply.github.com>
2020-06-30 19:44:43 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -13,7 +13,13 @@
  */
 require('dotenv').config();
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+const withSourceMaps = require('@zeit/next-source-maps');
+
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
 
 function foldLeft(...values) {
   return values.reduceRight((p, c) => c(p));
@@ -39,7 +45,7 @@ function withTSConfig(value) {
   };
 }
 
-const initialConfig = {
+const initialConfigDevelopment = {
   distDir: 'build',
   env: {
     API: process.env.API,
@@ -52,65 +58,31 @@ const initialConfig = {
     FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
     FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
   },
-  generateinDevMode: true,
-  workboxOpts: {
-    runtimeCaching: [
-      {
-        urlPattern: /^https:\/\/fonts\.gstatic\.com/,
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'google-fonts-webfonts',
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-          expiration: {
-            maxAgeSeconds: 60 * 60 * 24 * 365,
-            maxEntries: 30,
-          },
-        }
-      },
-      {
-        urlPattern: /\.(?:png|gif|jpg|jpeg|webp|svg)$/,
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'images',
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-          expiration: {
-            maxEntries: 60,
-            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-          },
-        },
-      },
-      {
-        urlPattern: /\.(?:js|css)$/,
-        handler: 'StaleWhileRevalidate',
-        options: {
-          cacheName: 'static-resources',
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-        }
-      },
-      {
-        urlPattern: /^https?.*/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'offlineCache',
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-          expiration: {
-            maxEntries: 200
-          }
-        }
-      },
-    ],
+};
+
+const initialConfigProduction = {
+  distDir: 'build',
+  env: {
+    API: process.env.API,
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
   },
 };
 
+const initialConfig =
+  process.env.DB_ENV !== 'production'
+    ? initialConfigDevelopment
+    : initialConfigProduction;
+
 module.exports = foldLeft(
+  withBundleAnalyzer,
+  withSourceMaps,
   withTSConfig,
   initialConfig,
 );
