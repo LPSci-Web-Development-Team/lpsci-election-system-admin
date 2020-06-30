a02bfc34fde00c2cbc65465f3545cb53dce5ee76
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-06-30 16:54:55 +0800

    Update theme preference

--- scoped-models/theme/ThemePreference.tsx
+++ scoped-models/theme/ThemePreference.tsx
@@ -2,9 +2,20 @@
 import createModel from '@lxsmnsyc/react-scoped-model';
 
 // ANCHOR Hooks
-import { useTheme } from '@functions/useTheme';
+import { useTheme } from '@lpsci/hooks';
 
-export const ThemePreference = createModel(() => useTheme(),
-  {
-    displayName: 'Models.ThemePreference',
-  });
+// ANCHOR Base
+import {
+  createTheme, lightThemePrimitives, createDarkTheme, darkThemePrimitives,
+} from 'baseui';
+
+// ANCHOR Themes
+import { LIGHT_THEME, DARK_THEME } from '@themes/theme';
+
+export const ThemePreference = createModel(() => useTheme(
+  createTheme(lightThemePrimitives, LIGHT_THEME),
+  createDarkTheme(darkThemePrimitives, DARK_THEME),
+),
+{
+  displayName: 'Models.ThemePreference',
+});
