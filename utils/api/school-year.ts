f2741f3f0c13d5583af7837c61507f212eda50b4
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-07-02 17:06:37 +0800

    Update school year api

--- utils/api/school-year.ts
+++ utils/api/school-year.ts
@@ -1,9 +1,20 @@
+// ANCHOR Interfaces
+import { IRequireSignIn } from '@interfaces/Common';
+
 // ANCHOR Payloads
 import { ICreateSchoolYear } from '@payloads/school-year';
 
 // ANCHOR Fetch
-import { POST } from '../fetch/methods';
+import { POST, GET } from '../fetch/methods';
 
+// ANCHOR Results
+import { ISchoolYearResult } from './results/school-year';
+
+/**
+ * ANCHOR: Create school year
+ *
+ * @param param0 Params
+ */
 export async function createSchoolYear({
   token, ...payload
 }: ICreateSchoolYear) {
@@ -13,4 +24,22 @@ export async function createSchoolYear({
       Authorization: `Bearer ${token}`,
     },
   });
+}
+
+/**
+ * ANCHOR: Get all school years
+ *
+ * @param param0 Params
+ */
+export async function getSchoolYears({
+  token,
+}: IRequireSignIn) {
+  const data = await GET('/school-year', {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return (await data.json()) as ISchoolYearResult[];
 }
