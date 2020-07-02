e5c0104a894ca51b6fec6ff511cfb9510ed7e56d
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-07-02 17:06:52 +0800

    Create students results

---
+++ utils/api/results/student.ts
@@ -0,0 +1,14 @@
+import { EGrade } from '@payloads/section';
+import { IUserResult } from './user';
+
+export interface IStudentCurrentResult {
+  readonly currentAdviser?: string;
+  readonly currentGradeLevel?: EGrade;
+  readonly currentSection?: string;
+}
+
+export interface IStudentResult extends IStudentCurrentResult {
+  readonly id: string;
+  readonly learnerReferenceNumber: string;
+  readonly user?: IUserResult;
+}
