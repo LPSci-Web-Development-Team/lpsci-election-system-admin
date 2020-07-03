7f01f8b59f58d0f7387f3cad54e6d5776b8f5cf1
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-07-04 04:03:12 +0800

    Make batch row actions dynamic

--- components/utils/DataTable/index.tsx
+++ components/utils/DataTable/index.tsx
@@ -11,7 +11,7 @@ import {
 import { Block } from 'baseui/block';
 
 // ANCHOR Interfaces
-import { IRow, IBatchDelete, IDelete } from '@interfaces/DataTable';
+import { IRow } from '@interfaces/DataTable';
 
 // ANCHOR Styles
 import { BLOCK } from './styles';
@@ -19,47 +19,22 @@ import { BLOCK } from './styles';
 interface IProps {
   columns: any[];
   rows: IRow[];
-  batchDelete?: (params: IBatchDelete) => any;
-  individualDelete?: (param: IDelete) => any;
+  batchActions?: BatchActionT[];
+  rowActions?: RowActionT[];
 }
 
 export const DataTable = React.memo(({
-  columns, rows, batchDelete, individualDelete,
-}: IProps) => {
-  const batchActions: BatchActionT[] = [];
-
-  const rowActions: RowActionT[] = [];
-
-  React.useEffect(() => {
-    if (batchDelete) {
-      batchActions.push(
-        {
-          label: 'Remove',
-          onClick: batchDelete,
-          renderIcon: Alert,
-        },
-      );
-    }
-
-    if (individualDelete) {
-      rowActions.push(
-        {
-          label: 'Remove',
-          onClick: individualDelete,
-          renderIcon: Alert,
-        },
-      );
-    }
-  }, [batchActions, batchDelete, individualDelete, rowActions]);
-
-  return (
-    <Block overrides={BLOCK}>
-      <BaseDataTable
-        batchActions={batchActions}
-        rowActions={rowActions}
-        columns={columns}
-        rows={rows}
-      />
-    </Block>
-  );
-});
+  columns,
+  rows,
+  batchActions,
+  rowActions,
+}: IProps) => (
+  <Block overrides={BLOCK}>
+    <BaseDataTable
+      batchActions={batchActions}
+      rowActions={rowActions}
+      columns={columns}
+      rows={rows}
+    />
+  </Block>
+));
