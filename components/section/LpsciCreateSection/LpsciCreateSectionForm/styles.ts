f97a68f83e2d8114059709e8ff908f8323b3eda1
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-07-04 21:53:59 +0800

    Make forms responsive

--- components/section/LpsciCreateSection/LpsciCreateSectionForm/styles.ts
+++ components/section/LpsciCreateSection/LpsciCreateSectionForm/styles.ts
@@ -1,5 +1,10 @@
+import { BREAKPOINTS } from '@themes/breakpoints';
 import { StyleObject } from 'styletron-react';
 
 export const FORM: StyleObject = {
   width: '50%',
+
+  [BREAKPOINTS.small]: {
+    width: '100%',
+  },
 };
