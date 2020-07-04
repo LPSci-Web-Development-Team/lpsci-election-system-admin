7e988fe25bd38c764b8fab2c2d201ea89202d672
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-07-05 06:08:31 +0800

    Create individual party page

---
+++ components/party/LpsciIndividualParty/LpsciIndividualPartyTabs/LpsciIndividualPartyCandidates/LpsciIndividualPartyCandidate/index.tsx
@@ -0,0 +1,66 @@
+// ANCHOR React
+import * as React from 'react';
+
+// ANCHOR Next
+import { useRouter } from 'next/router';
+
+// ANCHOR Base
+import { ListItem, ListItemLabel } from 'baseui/list';
+import { Tag, KIND, VARIANT } from 'baseui/tag';
+import { Button, SHAPE, SIZE } from 'baseui/button';
+import { Show } from 'baseui/icon';
+
+// ANCHOR Results
+import { ICandidateResult } from '@api/results/candidate';
+
+// ANCHOR Styles
+import { LIST, TAG } from './styles';
+
+interface IProps {
+  data: ICandidateResult;
+}
+
+export const LpsciIndividualPartyCandidate = ({
+  data,
+}: IProps) => {
+  const router = useRouter();
+
+  const artwork = React.useCallback(() => (
+    <Tag
+      closeable={false}
+      kind={KIND.primary}
+      variant={VARIANT.solid}
+      overrides={TAG}
+    >
+      {data.position}
+    </Tag>
+  ), [data.position]);
+
+  const onClick = React.useCallback(() => (
+    router.push(`/candidate/view/${data.id}`)
+  ), [data.id, router]);
+
+  const endEnhancer = React.useCallback(() => (
+    <Button
+      onClick={onClick}
+      shape={SHAPE.round}
+      size={SIZE.compact}
+    >
+      <Show />
+    </Button>
+  ), [onClick]);
+
+  return (
+    <ListItem
+      artwork={artwork}
+      overrides={LIST}
+      endEnhancer={endEnhancer}
+    >
+      <ListItemLabel>
+        {data.student.user
+          ? `${data.student.user?.lastName}, ${data.student.user?.firstName}`
+          : data.student.learnerReferenceNumber}
+      </ListItemLabel>
+    </ListItem>
+  );
+};
