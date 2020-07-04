f9df4e9cf38df1639c9ffeaf386e36c803480fb1
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-07-04 14:01:57 +0800

    Add reusable tab title

---
+++ components/utils/TabTitle/index.tsx
@@ -0,0 +1,19 @@
+// ANCHOR React
+import * as React from 'react';
+
+// ANCHOR Base
+import { Block } from 'baseui/block';
+import { LabelSmall } from 'baseui/typography';
+
+// ANCHOR Styles
+import { BLOCK, TITLE } from './styles';
+
+interface IProps {
+  title: string;
+}
+
+export const TabTitle = React.memo(({ title }: IProps) => (
+  <Block overrides={BLOCK}>
+    <LabelSmall overrides={TITLE}>{title}</LabelSmall>
+  </Block>
+));
