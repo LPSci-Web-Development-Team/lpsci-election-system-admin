f5a7ea940975021a0ee3056c139b446173befce8
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-07-04 21:54:11 +0800

    Add fetch for all sections

--- utils/api/section.ts
+++ utils/api/section.ts
@@ -1,10 +1,14 @@
 // ANCHOR Payloads
 import { ICreateSection } from '@payloads/section';
 
+// ANCHOR Interfaces
+import { IRequireSignIn } from '@interfaces/Common';
+
 // ANCHOR Fetch
-import { POST } from '../fetch/methods';
+import { POST, GET } from '../fetch/methods';
 
 // ANCHOR Results
+import { ISectionResult } from './results/section';
 
 /**
  * ANCHOR: Create school year
@@ -20,4 +24,22 @@ export async function createSection({
       Authorization: `Bearer ${token}`,
     },
   });
+}
+
+/**
+ * ANCHOR: Get all school years
+ *
+ * @param param0 Params
+ */
+export async function getSections({
+  token,
+}: IRequireSignIn) {
+  const data = await GET('/section', {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return (await data.json()) as ISectionResult[];
 }
