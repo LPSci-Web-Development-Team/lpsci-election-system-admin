d4a3cb440f4626c7e403d3b11a6fe2ade3f0c115
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-07-06 03:39:14 +0800

    Update types

--- utils/api/results/student.ts
+++ utils/api/results/student.ts
@@ -1,6 +1,19 @@
 import { EGrade } from '@payloads/section';
 import { IUserResult } from './user';
 
+export interface ISchoolYear {
+  readonly id: string;
+  readonly year: string;
+}
+
+export interface ISection {
+  readonly id: string;
+  readonly name: string;
+  readonly gradeLevel: EGrade;
+  readonly adviser: string;
+  readonly schoolYear?: ISchoolYear;
+}
+
 export interface IStudentCurrentResult {
   readonly currentAdviser?: string;
   readonly currentGradeLevel?: EGrade;
@@ -11,4 +24,5 @@ export interface IStudentResult extends IStudentCurrentResult {
   readonly id: string;
   readonly learnerReferenceNumber: string;
   readonly user: IUserResult;
+  readonly sections?: ISection[];
 }
