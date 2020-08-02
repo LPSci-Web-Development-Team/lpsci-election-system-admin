9521ce59f38bdedba795a82a705c482d8bef535c
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-08-02 23:18:21 +0800

    Update CandidateData.tsx

--- scoped-models/candidate/CandidateData.tsx
+++ scoped-models/candidate/CandidateData.tsx
@@ -1,35 +1,14 @@
-// ANCHOR SWR
-import useSWR from 'swr';
-
 // ANCHOR Model
 import createModel from '@lxsmnsyc/react-scoped-model';
 
 // ANCHOR API
-import { getCandidates } from '@api/candidate';
 import { ICandidateResult } from '@api/results/candidate';
 
-// ANCHOR Hooks
-import { useAuthToken } from '@firebase/hooks/useAuthToken';
-
-interface IState {
-  data?: ICandidateResult[];
-  error: any;
-  mutate: () => Promise<ICandidateResult[] | undefined>;
+interface IProps {
+  data: ICandidateResult;
+  id: string;
 }
 
-export const CandidateData = createModel<IState>(() => {
-  const { data: token } = useAuthToken();
-
-  const { data, error, mutate } = useSWR(
-    (token ? '/candidate' : null),
-    () => (token ? getCandidates({ token }) : undefined),
-  );
-
-  return {
-    data,
-    error,
-    mutate,
-  };
-}, {
+export const CandidateData = createModel<IProps, IProps>((props) => props, {
   displayName: 'Models.CandidateData',
 });
